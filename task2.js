dc504fa2b656994d1e63d98344f1ccbb51b2475b
Aliaksei Akulau <aliaksei_akulau@epam.com>
2022-12-04 20:54:42 +0400

    fix: implement task3 with pipeline method

--- task2.js
+++ task2.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from "path";
 import csv from 'csvtojson';
 import url from 'url';
+import { pipeline } from 'stream';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const csvPath = path.join(__dirname, './csv/nodejs-hw1-ex1.csv');
@@ -19,10 +20,10 @@ const transform = csv().subscribe((json) => {
     toLowerKeys(json);
 });
 
-readStream.on('error', (err) => {
-    console.error(err.message)
-}).pipe(transform).on('error', (err) => {
-    console.error(err.message)
-}).pipe(writeStream).on('error', (err) => {
-    console.error(err.message)
-});
+const errorHandler = (err) => {
+    if (err) {
+        console.error(err.message)
+    }
+};
+
+pipeline(readStream, transform, writeStream, errorHandler);
